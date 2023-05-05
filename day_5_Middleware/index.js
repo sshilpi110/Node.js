aa4d0180d0b75b4f4ce86a5b02d8c35ba3aa26c3
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-05 20:58:36 +0530

    middleware

--- day_5_Middleware/index.js
+++ day_5_Middleware/index.js
@@ -2,8 +2,39 @@
 const express=require("express")
  const app= express() //invoking
 
+ 
+
 app.get("/",(req,res)=>{
+    console.log("home page")
     res.send("Home page")
+})
+
+
+app.use((req,res,next)=>{
+
+    console.log("hello from middleware")
+    if(req.url==="/about"){
+    next()
+    }
+    else{
+        res.send("not permitted")
+    }
+    console.log("by middleware")
+ })
+
+app.get("/about",(req,res)=>{
+    console.log("about page")
+    res.send("about page")
+})
+
+app.get("/contact",(req,res)=>{
+    console.log("contact page")
+    res.send("contact page")
+})
+
+app.get("/data",(req,res)=>{
+    console.log("data page")
+    res.send("data page")
 })
 
 app.listen(4200,()=>{
