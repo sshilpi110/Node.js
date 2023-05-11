98566103ff6a2ce29cdf1eac52ddb45f25b50252
shilpi <105913922+sshilpi110@users.noreply.github.com>
2023-05-11 22:22:49 +0530

    ok

--- day_7_mongoose/database.js
+++ day_7_mongoose/database.js
@@ -1,4 +1,4 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
 // const database=()=>{
 //     const connection=mongoose.connect("mongodb://127.0.0.1:27017/")
@@ -7,3 +7,50 @@ const mongoose=require("mongoose")
 // }
 
 // database() //synchronous function
+
+const database = async () => {
+    try {
+        const connection = await mongoose.connect("mongodb://127.0.0.1:27017/")
+        console.log("connected to mongo")
+        connection.disconnect()
+        console.log("disconnected")
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+// database()
+
+
+const main = async () => {
+    try {
+        const connection = await mongoose.connect("mongodb://127.0.0.1:27017/backened")
+        console.log("connected to database")
+        // await StudentModel.insertMany([{ name: "shilpi", age: 22, city: "patna" }, { name: "nayan", age: 45, city: "Delhi" }])
+        const student=new StudentModel({
+            name:"vijay cha",
+            age:23,
+            city:"gandhipur"
+        })
+        await student.save()
+        console.log("inserted the data")
+        
+        console.log("following is the data from db")
+        const students=await StudentModel.find({$and:[{age:{$gte:22}},{age:{$lte:45}}]})//to get the data from db to vs trminal
+        console.log(students)
+        connection.disconnect()
+        console.log("disconnected")
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+main() 
+
+const StudentSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    city: String
+})
+
+const StudentModel = mongoose.model("student", StudentSchema)
